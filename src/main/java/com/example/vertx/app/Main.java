a76dc4e885db152833b936618db5ddbe5d0d4c0c
LahiruTjay <lahiru.tjay@gmail.com>
2018-07-02 21:47:15 +0530

    MySQL DB connectivity added.

--- src/main/java/com/example/vertx/app/Main.java
+++ src/main/java/com/example/vertx/app/Main.java
@@ -1,6 +1,7 @@
 package com.example.vertx.app;
 
 import com.example.vertx.verticles.HttpServerVerticle;
+import com.example.vertx.verticles.SQLTestVerticle;
 import com.example.vertx.verticles.JDBCTestVerticle;
 
 import io.vertx.core.Vertx;
@@ -12,15 +13,16 @@ public class Main {
 	public static void main(String[] args) {
 		// VertxOptions options = new VertxOptions();
 		vertx = Vertx.vertx();
-		deployVerticle(HttpServerVerticle.class, "Http Vericle");
-		deployVerticle(JDBCTestVerticle.class, "JDBC Vericle");
+		deployVerticle(HttpServerVerticle.class, "Http Verticle");
+		//deployVerticle(JDBCTestVerticle.class, "JDBC Verticle");
+		deployVerticle(SQLTestVerticle.class, "MySQl Verticle");
 	}
 
 	private static void deployVerticle(Class<?> verticleClass, String name) {
 		vertx.deployVerticle(verticleClass.getName(), response -> {
 			if (response.succeeded()) {
 				String deploymentId = response.result();
-				System.out.println("Deployment Id : " + deploymentId);
+				System.out.println("Deployment Id : " + deploymentId + " : " + name);
 			} else {
 				response.cause().printStackTrace();
 			}
---
+++ src/main/java/com/example/vertx/verticles/SQLTestVerticle.java
@@ -0,0 +1,43 @@
+package com.example.vertx.verticles;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.sql.SQLConnection;
+
+public class SQLTestVerticle extends AbstractVerticle {
+
+	@Override
+	public void start() throws Exception {
+
+		final JDBCClient client = JDBCClient.createShared(vertx,
+				new JsonObject().put("url", "jdbc:mysql://localhost/test")
+						.put("driver_class", "com.mysql.cj.jdbc.Driver").put("user", "root").put("password", "root"));
+
+		client.getConnection(conn -> {
+
+			if (conn.failed()) {
+				System.err.println(conn.cause().getMessage());
+				return;
+			}
+
+			final SQLConnection connection = conn.result();
+
+			connection.query("SELECT * FROM test_table", result -> {
+
+				for (JsonArray line : result.result().getResults()) {
+					System.out.println(line.encode());
+				}
+
+				connection.close(done -> {
+					if (done.failed()) {
+						throw new RuntimeException(done.cause());
+					}
+				});
+			});
+		});
+
+	}
+
+}
